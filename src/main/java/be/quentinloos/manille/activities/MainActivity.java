e9f1c182a8992dcbe270d87b813457a634a7186c
Quentin Loos <contact@quentinloos.be>
2013-08-13 21:50:50 +0200

    MainActivity: Ajout

--- src/main/java/be/quentinloos/manille/activities/MainActivity.java
+++ src/main/java/be/quentinloos/manille/activities/MainActivity.java
@@ -3,7 +3,9 @@ package be.quentinloos.manille.activities;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import be.quentinloos.manille.R;
 import be.quentinloos.manille.core.Manille;
@@ -17,13 +19,25 @@ import be.quentinloos.manille.util.ScoreAdapter;
  */
 public class MainActivity extends Activity {
 
+    Manille manille;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Manille manille = new ManilleFree();
+        manille = new ManilleFree();
         manille.endTurns(25, 35);
+        manille.endTurns(25, 35);
+        manille.endTurns(25, 35);
+        manille.endTurns(25, 35);
+        manille.endTurns(25, 35);
+        manille.endTurns(25, 35);
+
+        TextView sum1 = (TextView) findViewById(R.id.sum1);
+        TextView sum2 = (TextView) findViewById(R.id.sum2);
+        sum1.setText(Integer.toString(manille.getScores()[0]));
+        sum2.setText(Integer.toString(manille.getScores()[1]));
 
         ListView lv = (ListView) findViewById(R.id.listView);
         ScoreAdapter sa = new ScoreAdapter(this, manille.getTurns());
@@ -36,6 +50,18 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
     }
 
 }
