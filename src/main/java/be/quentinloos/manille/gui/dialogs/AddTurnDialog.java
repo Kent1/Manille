c4d607c7c78d5ed6302ae463543d707f9637c9d5
Quentin Loos <contact@quentinloos.be>
2013-08-17 11:42:43 +0200

    AddTurnDialog: bug noTrump & add listener on checkbox

--- src/main/java/be/quentinloos/manille/gui/dialogs/AddTurnDialog.java
+++ src/main/java/be/quentinloos/manille/gui/dialogs/AddTurnDialog.java
@@ -10,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ import be.quentinloos.manille.gui.activities.MainActivity;
 public class AddTurnDialog extends DialogFragment {
 
     EditText pointsTeam1, pointsTeam2;
+    CheckBox noTrump1, noTrump2;
 
     public static AddTurnDialog newInstance(int title) {
         AddTurnDialog dialog = new AddTurnDialog();
@@ -51,6 +53,14 @@ public class AddTurnDialog extends DialogFragment {
         pointsTeam1.addTextChangedListener(getTextWatcher(pointsTeam1, pointsTeam2));
         pointsTeam2.addTextChangedListener(getTextWatcher(pointsTeam2, pointsTeam1));
 
+        // Checkbox no-trump suit
+        noTrump1 = (CheckBox) view.findViewById(R.id.no_trump1);
+        noTrump2 = (CheckBox) view.findViewById(R.id.no_trump2);
+
+        // Checkboxes can't be both checked
+        noTrump1.setOnCheckedChangeListener(getOnCheckedChangeListener(noTrump2));
+        noTrump2.setOnCheckedChangeListener(getOnCheckedChangeListener(noTrump1));
+
         return new AlertDialog.Builder(this.getActivity())
                 .setTitle(title)
                 .setView(view)
@@ -61,8 +71,8 @@ public class AddTurnDialog extends DialogFragment {
                     public void onClick(DialogInterface dialog, int id) {
                         int score1 = 0, score2 = 0;
                         try {
-                            score1 = Integer.parseInt(((EditText) view.findViewById(R.id.turn_score1)).getText().toString());
-                            score2 = Integer.parseInt(((EditText) view.findViewById(R.id.turn_score2)).getText().toString());
+                            score1 = Integer.parseInt(pointsTeam1.getText().toString());
+                            score2 = Integer.parseInt(pointsTeam2.getText().toString());
                         } catch (NumberFormatException e) {
                             AddTurnDialog.this.getDialog().cancel();
                             Toast.makeText(view.getContext(), getString(R.string.exception_score), Toast.LENGTH_SHORT).show();
@@ -77,8 +87,12 @@ public class AddTurnDialog extends DialogFragment {
                             Turn turn = new Turn(score1, score2, (noTrump1 || noTrump2 ? Turn.Trump.NOTRUMP : null), double1, double2, 0);
                             Manille manille = ((MainActivity) getActivity()).getManille();
                             manille.addTurn(turn);
-                            if (manille instanceof ManilleTurns)
-                                ((ManilleTurns) manille).addNoTrumpTurn(1);
+                            if (manille instanceof ManilleTurns) {
+                                if (noTrump1)
+                                    ((ManilleTurns) manille).addNoTrumpTurn(1);
+                                else if (noTrump2)
+                                    ((ManilleTurns) manille).addNoTrumpTurn(2);
+                            }
                             ((MainActivity) getActivity()).refreshMainFragment();
                         } catch (IllegalArgumentException e) {
                             Toast.makeText(getActivity(), getString(R.string.exception_score), Toast.LENGTH_SHORT).show();
@@ -129,4 +143,15 @@ public class AddTurnDialog extends DialogFragment {
             public void afterTextChanged(Editable s) {}
         };
     }
+
+    private CompoundButton.OnCheckedChangeListener getOnCheckedChangeListener(final CheckBox noTrump2) {
+        return new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && noTrump2.isChecked())
+                    noTrump2.setChecked(false);
+            }
+        };
+    }
+
 }
