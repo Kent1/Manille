566e557df3df75571b6c95c702372d8dc7ed0d4a
Quentin Loos <contact@quentinloos.be>
2013-08-17 11:12:49 +0200

    Turn: corrections and cleaning

--- src/main/java/be/quentinloos/manille/core/Turn.java
+++ src/main/java/be/quentinloos/manille/core/Turn.java
@@ -27,13 +27,13 @@ public class Turn {
      * @param trump The trump chosen.
      * @param double1 If the team1 have doubled ?
      * @param double2 If the team2 have doubled ?
-     * @param drawInLastPlay Number of draw play before.
+     * @param reportedMult Number of draw play before.
      */
-    public Turn(int points1, int points2, Trump trump, boolean double1, boolean double2, int drawInLastPlay) {
+    public Turn(int points1, int points2, Trump trump, boolean double1, boolean double2, int reportedMult) {
         if (points1 < 0 || points1 > 60 || points2 < 0 || points2 > 60 || points1 + points2 != 60)
             throw new IllegalArgumentException("Bad number of points");
 
-        this.mult = computeMult(points1, points2, trump, double1, double2, drawInLastPlay);
+        this.mult = computeMult(points1, points2, trump, double1, double2, reportedMult);
 
         this.trump = trump;
 
@@ -41,13 +41,25 @@ public class Turn {
         this.points2 = (points2 > 29) ? (points2 - 30) * mult : 0;
     }
 
+    public Turn(int points1, int points2, Trump trump) {
+        this(points1, points2, trump, false, false, 0);
+    }
+
+    public Turn(int points1, int points2, Trump trump, boolean double1, boolean double2) {
+        this(points1, points2, trump, double1, double2, 0);
+    }
+
+    public Turn(int points1, int points2, Trump trump, int reportedMult) {
+        this(points1, points2, trump, false, false, reportedMult);
+    }
+
     /**
      * Computes the factor of multiplication for the turn.
      *
      * @return The factor of multiplication
      */
-    private int computeMult(int points1, int points2, Trump trump, boolean double1, boolean double2, int drawInLastPlay) {
-        int mult = 0;
+    private int computeMult(int points1, int points2, Trump trump, boolean double1, boolean double2, int reportedMult) {
+        int mult = 1;
 
         if (points1 == 60 || points2 == 60)
             mult++;
@@ -58,37 +70,21 @@ public class Turn {
         if (double1) mult++;
         if (double2) mult++;
 
-        mult += drawInLastPlay;
+        mult += reportedMult;
 
         return mult;
-    }
-
-    public Turn(int points1, int points2, Trump trump) {
-        this(points1, points2, trump, false, false, 0);
     }
 
     public int getPoints1() {
         return points1;
     }
 
-    public void setPoints1(int points1) {
-        this.points1 = points1;
-    }
-
     public int getPoints2() {
         return points2;
     }
 
-    public void setPoints2(int points2) {
-        this.points2 = points2;
-    }
-
     public Trump getTrump() {
         return trump;
-    }
-
-    public void setTrump(Trump trump) {
-        this.trump = trump;
     }
 
     public int getMult() {
